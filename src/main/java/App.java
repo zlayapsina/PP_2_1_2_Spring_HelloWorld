200586729094e4598fa8f6690df736ffa1736fad
zlayapsina <alex.tsaregorodcev1@gmail.com>
2023-10-24 17:00:35 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println((bean == bean2));
+        System.out.println((catBean == catBean2));
     }
 }
